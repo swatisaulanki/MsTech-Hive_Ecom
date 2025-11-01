9a6bcd4ecfa7f00358305d037a991e14d0f38273
swatisaulanki <swatisaulanki@gmail.com>
2025-11-01 05:35:50 +0530

    updated name

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { addOrder } from "../controllers/orderController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { protect } from "../middleware/auth.js"
 
 const router = express.Router();
 
